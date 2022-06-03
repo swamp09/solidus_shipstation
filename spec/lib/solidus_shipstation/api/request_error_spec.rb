67187196902a2827311f3ea5c7546e13435266a0
Nick Belzer <me@nickbelzer.me>
2022-06-03 18:01:48 +0200

    Update stubbed error body to resemble real ShipStation error

--- spec/lib/solidus_shipstation/api/request_error_spec.rb
+++ spec/lib/solidus_shipstation/api/request_error_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SolidusShipstation::Api::RequestError do
         'HTTParty::Response',
         code: 500,
         headers: { 'Key' => 'Value' },
-        body: '{ "message": "Internal Server Error" }',
+        body: '{ "Message": "An error has occured.", "ExceptionMessage": "orders are required", "ExceptionType": "System.ArgumentException" }',
       )
 
       error = described_class.from_response(response)
@@ -13,7 +13,7 @@ RSpec.describe SolidusShipstation::Api::RequestError do
       expect(error).to have_attributes(
         response_code: 500,
         response_headers: { 'Key' => 'Value' },
-        response_body: '{ "message": "Internal Server Error" }',
+        response_body: '{ "Message": "An error has occured.", "ExceptionMessage": "orders are required", "ExceptionType": "System.ArgumentException" }',
       )
     end
   end
